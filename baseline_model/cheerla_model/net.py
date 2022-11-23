57b55005c31a005af494da0a8202ee99741a374b
ZhangqiJiang07 <111237771+ZhangqiJiang07@users.noreply.github.com>
2022-11-23 09:40:06 +0800

    Add files via upload

---
+++ baseline_model/cheerla_model/net.py
@@ -0,0 +1,142 @@
+"""Pancancer survival prediction using a deep learning architecture with multimodal representation and integration"""
+
+# import sys
+# sys.path.append('/sub_models')
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+from sub_models.mirnaNet import MirnaNet
+from sub_models.mrnaNet import MrnaNet
+from sub_models.cnvNet import CnvNet
+from sub_models.clinicalEmbeddingNet import ClinicalEmbeddingNet
+from sub_models.attention import Attention
+from sub_models.fixedAttention import FixedAttention
+from sub_models.sumFusion import AverageFusion
+
+class Net(nn.Module):
+	def __init__(self, modalities, m_length, fusion_method='attention', device=None,
+					input_modality_dim={'clinical':4, 'mRNA':1579, 'miRNA':743, 'CNV':2711}):
+		"""
+		Parameters
+		----------
+		modalities: list
+			Used modalities.
+
+		m_length: int
+			Representation length.
+
+		"""
+		super(Net, self).__init__()
+		self.data_modalities = modalities
+		self.m_length = m_length
+		self.dim = input_modality_dim
+		self.device = device
+
+		self.submodel_pipeline = {}
+		# clinical -----------------------------------------------#
+		if 'clinical' in self.data_modalities:
+			self.clinical_submodel = ClinicalEmbeddingNet(m_length=self.m_length, device=self.device)
+			self.submodel_pipeline['clinical'] = self.clinical_submodel
+
+		# mRNA ---------------------------------------------------#
+		if 'mRNA' in self.data_modalities:
+			self.mRNA_submodel = MrnaNet(mrna_length=self.dim['mRNA'], m_length=self.m_length, device=self.device)
+			self.submodel_pipeline['mRNA'] = self.mRNA_submodel
+
+		# miRNA --------------------------------------------------#
+		if 'miRNA' in self.data_modalities:
+			self.miRNA_submodel = MirnaNet(mirna_length=self.dim['miRNA'], m_length=self.m_length, device=self.device)
+			self.submodel_pipeline['miRNA'] = self.miRNA_submodel
+
+		# CNV ----------------------------------------------------#
+		if 'CNV' in self.data_modalities:
+			self.CNV_submodel = CnvNet(cnv_length=self.dim['CNV'], m_length=self.m_length, device=self.device)
+			self.submodel_pipeline['CNV'] = self.CNV_submodel
+
+		# Fusion -------------------------------------------------#
+		if len(self.data_modalities) > 1:
+			if fusion_method == 'attention':
+				self.fusion = Attention(m_length=self.m_length, modalities=self.data_modalities, device=self.device)
+			elif fusion_method == 'fixedAttention':
+				self.fusion = FixedAttention(m_length=self.m_length, modalities=self.data_modalities, device=self.device)
+			elif fusion_method == 'average':
+				self.fusion = AverageFusion(m_length=self.m_length, modalities=self.data_modalities, device=self.device)
+
+
+		# Survival prediction
+		self.hazard_layer1 = nn.Linear(m_length, 1)
+
+		self.label_layer1 = nn.Linear(m_length, 2)
+
+
+	def forward(self, x):
+		"""
+		Parameters
+		----------
+		x: dictionary
+			Input data from different modality, like:
+			{'clinical': tensor, 'mRNA':tensor, }
+		"""
+		# Extract representations from different modality
+		representation = {}
+		flag = 1
+		for modality in x:
+			if modality in ['clinical_categorical', 'clinical_continuous']:
+				if flag:
+					representation['clinical'] = self.submodel_pipeline['clinical'](x['clinical_categorical'], x['clinical_continuous'])
+					flag = 0
+				continue
+			representation[modality] = self.submodel_pipeline[modality](x[modality])
+		# fusion part
+		if len(self.data_modalities) > 1:
+			x = self.fusion(representation)
+		else:
+			x = representation[self.data_modalities[0]]
+
+		# survival predict
+		hazard = self.hazard_layer1(x)
+
+		score = F.log_softmax(self.label_layer1(x), dim=1)
+
+		return {'hazard':hazard, 'score':score}, representation
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ baseline_model/cheerla_model/utils.py
@@ -0,0 +1,144 @@
+"""Utils"""
+
+import os
+
+import pandas as pd
+import numpy as np
+import torch
+from torch.utils.data.sampler import SubsetRandomSampler
+from torch.utils.data import DataLoader
+
+
+# The dictionary of the input data path
+DATA_PATH = {'clinical': 'Pc_clinical_emb.csv',
+			'mRNA': 'PC_mRNA_threshold_7.csv',
+			'miRNA': 'PC_miRNA.csv',
+			'CNV': 'PC_CNV_threshold_20.csv'}
+
+
+def setup_seed(seed):
+	"""
+	Set random seed for torch.
+	"""
+	torch.manual_seed(seed)
+	torch.cuda.manual_seed(seed)
+	np.random.seed(seed)
+	torch.backends.cudnn.deterministic = True
+
+def test_gpu():
+	"""
+	Detect the hardware: GPU or CPU?
+	"""
+	print('GPU？', torch.cuda.is_available())
+	device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+	print('The device is ', device)
+	return device
+
+def evaluate_model(c_index_arr):
+	"""
+	Calculate the Mean and the Standard Deviation about c-index array.
+	"""
+	m = np.sum(c_index_arr, axis=0) / len(c_index_arr)
+	s = np.std(c_index_arr)
+	return m, s
+
+
+
+def get_dataloaders(mydataset, train_sampler, val_sampler, test_sampler, batch_size):
+	"""
+	Parameters
+	----------
+	mydataset: Dataset
+
+	train_sampler: array
+		Patient indexs in train set.
+
+	val_sampler: array
+		Patient indexs in validation set.
+
+	test_sampler: array
+		Patient indexs in test set.
+
+	batch_size: int
+		Number of patients in each batch.
+
+	Return
+	------
+	A dictionary of train/validation/test set, like
+		{'train': train_loader, 'val': val_loader, 'test': test_loader}
+	"""
+	
+	dataloaders = {}
+	dataloaders['train'] = DataLoader(mydataset, batch_size=batch_size, sampler=SubsetRandomSampler(train_sampler))
+	dataloaders['val'] = DataLoader(mydataset, batch_size=len(val_sampler), sampler=SubsetRandomSampler(val_sampler))
+	dataloaders['test'] = DataLoader(mydataset, batch_size=len(test_sampler), sampler=SubsetRandomSampler(test_sampler))
+
+	print('Dataset sizes (# patients):')
+	print('train: ', len(train_sampler))
+	print('  val: ', len(val_sampler))
+	print(' test: ', len(test_sampler))
+	print()
+	print('Batch size: ', batch_size)
+
+	return dataloaders
+
+
+def compose_run_tag(model, lr, dataloaders, log_dir, suffix=''):
+	"""
+	Make the tag about modality and learning rate.
+	"""
+	def add_string(string, addition, sep='_'):
+		if not string:
+			return addition
+		else: return string + sep + addition
+
+	data = None
+	for modality in model.data_modalities:
+		data = add_string(data, modality)
+
+	run_tag = f'{data}_lr{lr}'
+
+	run_tag += suffix
+
+	print(f'Run tag: "{run_tag}"')
+
+	tb_log_dir = os.path.join(log_dir, run_tag)
+
+	return run_tag
+
+
+def save_5fold_results(c_index_arr, run_tag):
+	"""
+	Save the results after 5 fold cross validation.
+	"""
+	m, s = evaluate_model(c_index_arr)
+	with open(f'proposed_{run_tag}.txt', 'w') as file:
+		file.write(str(c_index_arr))
+		file.write(f"\n Mean: {m}")
+		file.write(f"\n Std: {s}")
+	file.close()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
